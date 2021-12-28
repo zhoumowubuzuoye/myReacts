c6d4c83914244453083c34207f6b48506b26ae1a
zhoumowubuzuoye <www.1078637049@qq.com>
2021-12-28 16:08:13 +0800

    新文件

---
+++ react-new/city/src/views/homecure/index.jsx
@@ -0,0 +1,14 @@
+/*
+ *@author: xiewenhao
+ *@date: 2021-12-27 14:31:08
+ *@version: V1.0.0
+ *@description:
+ */
+import React from 'react'
+
+function HomeCure() {
+  return <div>111</div>
+}
+
+export default HomeCure
+    
